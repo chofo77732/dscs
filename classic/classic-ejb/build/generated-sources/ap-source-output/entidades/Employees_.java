8d3a8eb91c16b44acd44717256c7d690bdbd40dd
iobanadenis <idra4797@gmail.com>
2017-08-10 10:14:43 -0500

    IO
    actualizacion

--- classic/classic-ejb/build/generated-sources/ap-source-output/entidades/Employees_.java
+++ classic/classic-ejb/build/generated-sources/ap-source-output/entidades/Employees_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-08-08T20:38:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-08-10T09:24:25")
 @StaticMetamodel(Employees.class)
 public class Employees_ { 
 
